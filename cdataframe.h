3f759a0ba7c9ab9faadebe5cd720b7699332c0ae
sawken6119 <166221724+sawken6119@users.noreply.github.com>
2024-04-20 14:01:39 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#define REALOCSIZE 256
+#ifndef CDATAFRAME_CDATAFRAME_H
+#define CDATAFRAME_CDATAFRAME_H
+int taille_logique_max;
 
 
 void remplissage_utilisateur(COLUMN **CDataframe, int nombre_de_colonnes);
