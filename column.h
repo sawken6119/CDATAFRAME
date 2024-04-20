858eb7644664768e8918ef4b44720ccba798ab63
Xenolgd <152002293+Xenolgd@users.noreply.github.com>
2024-04-20 12:41:40 +0200

    Update column.h

--- column.h
+++ column.h
@@ -1,7 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 #include <time.h>
+#include <float.h>
+#include <limits.h>
 #define REALOCSIZE 256
 #ifndef CDATAFRAME_CDATAFRAME_H
 #define CDATAFRAME_CDATAFRAME_H
+int taille_logique_max;
 
 
 // Définir une énumération pour représenter les différents types de données
@@ -21,4 +28,4 @@ typedef struct {
     DataType type; // Type de données de la colonne
 } COLUMN;
 
-COLUMN *create_column(char* title);
+COLUMN *create_column(char* title, DataType type);
