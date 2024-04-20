535c49b43aea92f8d9887c1bde1bff06c999ebed
sawken6119 <166221724+sawken6119@users.noreply.github.com>
2024-04-20 14:13:32 +0200

    Update column.c

--- column.c
+++ column.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#define REALOCSIZE 256
+#ifndef CDATAFRAME_CDATAFRAME_H
+#define CDATAFRAME_CDATAFRAME_H
+int taille_logique_max;
