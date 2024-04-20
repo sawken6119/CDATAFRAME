fdb354ff2f7b8bb1a5b3472441a1d67796ee7c47
Xenolgd <152002293+Xenolgd@users.noreply.github.com>
2024-04-20 12:14:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,29 +4,65 @@
 #include "CDataframe.h"
 #define OUI 0
 #define NON 1
-int main(void)
+
+int main()
 {
-    char titre[100];
-    int t_log,t_log1;
-    int Reponse;
-    printf("Donner un titre à la colonne : ");
-    scanf("%s", titre);
-    COLUMN *col = create_column(titre);
-    printf("Nombre de valeurs à saisir : ");
-    scanf("%d", &t_log);
-    for (int i = 0; i < t_log; i++)
+    COLUMN **CDataframe = NULL;
+
+    int *nombre_de_colonnes = malloc(sizeof(int));
+    printf("Entrer le nombre de colonnes du DataFrame ");
+    scanf("%d",nombre_de_colonnes);
+
+    CDataframe = malloc(*nombre_de_colonnes*sizeof (COLUMN*));
+
+    printf("\n");
+    int choix;
+    printf("Voulez vous un remplissage avec vos valeurs (1) ou un remplissage automatique(2) ? ");
+    scanf("%d",&choix);
+    while(choix!=1 && choix!=2)
     {
-        int value;
-        printf("Saisir la valeur %d : ", i+1);
-        scanf("%d", &value);
-        insert_value(col, value);
+        printf("Choisissez (1) ou (2) ? ");
+        scanf("%d",&choix);
+    }
+    if(choix == 1)
+        remplissage_utilisateur(CDataframe,*nombre_de_colonnes);
+    else
+        remplissage_dur(CDataframe,*nombre_de_colonnes);
+    printf("\n");
+    printf("Voici le tableau rempli :");
+    printf("\n");
+    print_DataFrame(CDataframe,*nombre_de_colonnes);
+    printf("\n");
+    print_partial_row_data(CDataframe, *nombre_de_colonnes, 2, 4);
+    printf("\n");
+    print_partial_column_data(CDataframe, *nombre_de_colonnes, 1, 2);
+    printf("\n");
+
+    // Exemple d'ajout d'une ligne de valeurs au DataFrame
+    void* values[*nombre_de_colonnes];
+
+    for(int i=0;i<*nombre_de_colonnes;++i)
+    {
+        if(CDataframe[i]->type!= STRING)
+        {
+            values[i] = &(CDataframe[i]->Tab[0]); // Tableau de valeurs à ajouter correspondant aux valeurs
+        }
+
+        else
+            values[i] = ((char**)(CDataframe[i]->Tab))[0]; // Tableau de valeurs à ajouter correspondant aux valeurs
+
     }
 
-    printf("Titre de la colonne : %s\n", col->title);
-    printf("Valeurs de la colonne : ");
-    for (int i = 0; i < col->t_log; i++) {
-        printf("%d ", col->Tab[i]);
+    add_row(CDataframe, *nombre_de_colonnes, values);
+    print_DataFrame(CDataframe,*nombre_de_colonnes);
+    int max_val = CDataframe[0]->t_log;
+    for (int i = 1; i < *nombre_de_colonnes; ++i)
+    {
+        if(max_val < CDataframe[i]->t_log)
+            max_val = CDataframe[i]->t_log;
     }
+    delete_row(CDataframe, *nombre_de_colonnes, max_val);
     printf("\n");
-    return 0;
+    print_DataFrame(CDataframe,*nombre_de_colonnes);
+
 }
