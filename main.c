ff9697dbf6db6211593deb99a57fbaaead0e7390
sawken6119 <166221724+sawken6119@users.noreply.github.com>
2024-05-10 19:34:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,22 +5,36 @@
 #define REALOC_SIZE 256
 
 
-int main() 
-{
+int main() {
     int REPONSE;
     char titre[100];
-    printf("Saisir un titre ? : ");
-    scanf("%s",&titre);
+    int nombre_colonnes;
+
+    printf("Combien de colonnes souhaitez-vous creer ? : ");
+    scanf("%d", &nombre_colonnes);
+
     Cdataframe *df = create_cdataframe();
-    ajout_colonne(df, titre);
-    //int valeurs[] = {1, 2, 3 , 4, 5, 6}; // Exemple de valeurs pour une nouvelle ligne
-    //ajout_ligne(df, valeurs);
+
+    for (int i = 0; i < nombre_colonnes; i++) {
+        printf("Saisir le titre de la colonne %d : ", i+1);
+        scanf("%s", titre);
+        ajout_colonne(df, titre);
+    }
+
+    // Remplissage du Cdataframe avec des valeurs
     remplir_Cdata(df);
+
+    // Affichage du nombre de colonnes et de lignes
     printf("Nombre de colonnes : %d\n", nb_col(df));
-    printf("Nombre de lignes : %d\n",nb_ligne(df));
-    int value = 5; // Exemple de valeur à comparer
+    printf("Nombre de lignes : %d\n", nb_ligne(df));
+
+    // Exemple de comparaison de valeurs
+    int value = 5;
     printf("Nombre de valeurs supérieures à %d : %d\n", value, nb_sup(df, value));
+
     // Affichage du contenu du Cdataframe
     afficher_Cdata(df);
+
     return 0;
 }
+
