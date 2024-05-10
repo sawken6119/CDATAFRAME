4660b65fcdcfe93e112bb9a10fd77086a377afc3
sawken6119 <166221724+sawken6119@users.noreply.github.com>
2024-05-10 19:20:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,17 +5,16 @@
 #define REALOC_SIZE 256
 
 
-int main() {
-    // Création d'un Cdataframe
+int main() 
+{
+    int REPONSE;
+    char titre[100];
+    printf("Saisir un titre ? : ");
+    scanf("%s",&titre);
     Cdataframe *df = create_cdataframe();
-    ajout_colonne(df, "Alexandre");
-    ajout_colonne(df, "Armand");
-    ajout_colonne(df, "Jules");
-    ajout_colonne(df, "Kassav");
-    ajout_colonne(df, "Rencard");
-    ajout_colonne(df, "Tuvol");
-    int valeurs[] = {1, 2, 3 , 4, 5, 6}; // Exemple de valeurs pour une nouvelle ligne
-    ajout_ligne(df, valeurs);
+    ajout_colonne(df, titre);
+    //int valeurs[] = {1, 2, 3 , 4, 5, 6}; // Exemple de valeurs pour une nouvelle ligne
+    //ajout_ligne(df, valeurs);
     remplir_Cdata(df);
     printf("Nombre de colonnes : %d\n", nb_col(df));
     printf("Nombre de lignes : %d\n",nb_ligne(df));
@@ -23,11 +22,5 @@ int main() {
     printf("Nombre de valeurs supérieures à %d : %d\n", value, nb_sup(df, value));
     // Affichage du contenu du Cdataframe
     afficher_Cdata(df);
-
-    // Libération de la mémoire allouée pour le Cdataframe
-    // (À faire à la fin du programme pour éviter les fuites de mémoire)
-    // ...
-    // free(df);
-
     return 0;
 }
