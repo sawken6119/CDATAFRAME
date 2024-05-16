a164b1e500e310ef0e77722fa048e29159a59b9e
sawken6119 <166221724+sawken6119@users.noreply.github.com>
2024-05-16 16:46:10 +0200

    Update column.h

--- column.h
+++ column.h
@@ -21,31 +21,3 @@ void value_sup(Column* col, int value);
 void value_inf(Column* col, int value);
 void value_egal(Column* col, int value);
 
-//Cdata
-typedef struct
-{
-    int nb_colone;
-    Column **col;
-}Cdataframe;
-Cdataframe *create_cdataframe();
-void inser_value(Cdataframe *df, Column* col);
-void remplir_col(Column *col);
-void remplir_Cdata(Cdataframe *df);
-void remplir_Cdata_dur(Cdataframe *df);
-void afficher_Cdata(Cdataframe *df);
-void affiche_partie_ligne(Cdataframe *df,int arret);
-void affiche_partie_col(Cdataframe *df,int arret);
-void ajout_ligne(Cdataframe *df,int *value);
-void del_ligne(Cdataframe *df,int choix);
-void ajout_colonne(Cdataframe *df, char *titre);
-void del_col(Cdataframe *df, int choix);
-void renommer_colonne(Cdataframe *df, int choix, char *nouveau_titre);
-int valeur_existante(Cdataframe *df, int value);
-void acces_valeur_cellule(Cdataframe *df, int choix_ligne, int choix_colonne, int new_value);
-void afficher_noms_colonnes(Cdataframe *df);
-int nb_ligne(Cdataframe *df);
-int nb_col(Cdataframe *df);
-int nb_egal(Cdataframe *df, int value);
-int nb_sup(Cdataframe *df, int value);
-int nb_inf(Cdataframe *df, int value);
-#endif //C_DATA_FRAME_COLUMN_H
