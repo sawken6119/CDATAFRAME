04fca6a42965dbe15d8d7324b49d72432d28b5af
sawken6119 <166221724+sawken6119@users.noreply.github.com>
2024-05-18 18:27:28 +0200

    Update main.c

--- main.c
+++ main.c
@@ -27,7 +27,23 @@ int main() {
     // Affichage du nombre de colonnes et de lignes
     printf("Nombre de colonnes : %d\n", nb_col(df));
     printf("Nombre de lignes : %d\n", nb_ligne(df));
+    printf("Quelle colonne souhaitez-vous supprimer ? : ", nombre_colonnes);
+    scanf("%d", &choix);
+    if (choix < 1 || choix > nombre_colonnes)
+    {
+        printf("D'accord.\n");
+    } else
+    {
+        // Supprimer la colonne choisie
+        delete_column(&(df->col[choix - 1]));
 
+        // Réorganiser les colonnes restantes
+        for (int i = choix - 1; i < nombre_colonnes - 1; i++)
+        {
+            df->col[i] = df->col[i + 1];
+        }
+        df->nb_colone--;  // Mettre à jour le nombre de colonnes
+    }
     // Exemple de comparaison de valeurs
     int value = 5;
     printf("Nombre de valeurs supérieures à %d : %d\n", value, nb_sup(df, value));
